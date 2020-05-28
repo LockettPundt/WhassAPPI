c019b465db802d6ffea8a00951456a40f12b8e21
Lockett Pundt <kalifornium@hotmail.com>
2020-05-28 15:21:09 -0400

    this isn't doing anything yet but will be the main endpoint to create and join rooms. i'll change the name to better reflect it's purpose.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const socketAPI = require('../utils/socketAPI');
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
+  socketAPI.io.emit('greet', 'hey you');
   res.sendStatus(200);
 });
 
