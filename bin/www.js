ac7fb64508c3aac45bcdb425db7d4714ef71bcb4
Lockett Pundt <kalifornium@hotmail.com>
2020-05-28 15:18:10 -0400

    updated how the messages are returned and processed in www. changed the filename to .js since VScode was having issues with the file type?

---
+++ bin/www.js
@@ -0,0 +1,108 @@
+/* eslint-disable import/order */
+// #!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+require('dotenv').config();
+
+
+const debug = require('debug')('whasssappapi:server');
+const http = require('http');
+const app = require('../app');
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+
+io.on('connection', (socket) => {
+  socket.on('join', (data) => {
+    console.log(data);
+    console.log(`${data.firstName} has joined ${data.chatRoom}`);
+    socket.join(data.chatRoom);
+    io.emit('roomJoined', data.chatRoom);
+  });
+  socket.on('message', (data) => {
+    console.log(data);
+    io.emit('newMessage', data);
+  });
+});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
