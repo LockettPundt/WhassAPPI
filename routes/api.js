25e13e89119ac786e28b3db541efbd9ef03239db
Lockett Pundt <kalifornium@hotmail.com>
2020-06-09 15:35:21 -0400

    initial message post route that pushes the message along with author, time and chatRoom to the DB. initial get message history route that pulls the chat history from the DB according to what room name the person is logged into.I will flesh out some edge cases and error handling messages.

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcyrpt = require('bcrypt');
+const moment = require('moment');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
+const MessageModel = require('../models/MessageModel');
+
+// create chat room.
+
+router.post('/create', (req, res) => {
   res.sendStatus(200);
 });
+
+// join chat room
+
+router.post('/join', (req, res) => {
+  res.send(200);
+});
+
+router.post('/messages', async (req, res) => {
+  const {
+    userName, chatRoomName, message, messageTime,
+  } = req.body;
+  // console.log(req.body);
+
+  const newMessage = new MessageModel({
+    userName,
+    chatRoomName,
+    message,
+    messageTime,
+  });
+  try {
+    // console.log('posting: ', newMessage);
+    const postMessage = await newMessage.save();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// get message history
+
+router.get('/chathistory/:room?', async (req, res) => {
+  const { room } = req.params;
+  // console.log(room);
+  const chatHistory = await MessageModel.find({ chatRoomName: room });
+  // console.log('chat history is: ', chatHistory);
+  res.json(chatHistory);
+});
+
 
 module.exports = router;
